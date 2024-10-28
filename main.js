a7a9ac070688afd0bf30180eda060a3f30b2e6ff
17alexx <ulvia91@icloud.com>
2024-10-28 17:30:55 +0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,72 @@
+// let num = 0 
+//  if(num > 0){
+//      console.log("musbetdi");
+//  }else if(num < 0){
+//      console.log("menfidi");
+//  }else if(num == 0){
+//      console.log("sifir");  
+//  }
+
+
+
+
+// let adam = "admin";
+
+// switch (adam) {
+//     case "admin":
+//         console.log("admin hesabi ile girdiniz");
+//         break;
+//     case "istifadeci":
+//         console.log("istifadeci ile daxil oldunuz");
+//         break;
+//     case "qonaq":
+//         console.log("qonaq hesabiyla daxil oldunuz");
+
+// }
+
+
+
+// let number = prompt("yoxlayin")
+// if(number %2 == 0){
+//     console.log(number , "cutdur");   
+// }else{
+//     console.log(number , "tekdir");
+// }
+
+
+
+// let number = prompt("reqem yazin")
+// console.log(number !== 10);
+
+// let number = prompt("reqem yaz")
+//  if(number > 5 & number < 10){
+//      console.log("5den boyuk,10dan kicik deyil");
+//  }else{
+//      console.log("5den boyuk,10dan kicikdiir");
+//  }
+
+
+// let number1 = 1
+// let number2 = 1
+// if (number1 && number2) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+// let number1 = 7 < 6
+// let number2 = 2 < 9
+// if (number1 || number2) {
+//     console.log("true");
+// } else {
+//     console.log("false");
+// }
+
+
+
+let number = 5;
+if (number > 0 && number % 2 == 0) {
+    console.log("musbet ve cutdur");
+} else {
+    console.log("ne musbet nede cutdu");
+}
